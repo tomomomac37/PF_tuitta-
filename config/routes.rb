84a29cd52cbc3f6e1c7a97c782564259b4bb7dd9
tomomomac37 <tomomomac37@gmail.com>
2021-09-28 15:07:37 +0000

    posts/indexレイアウトnavvar作成中

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get 'posts/index'
-  root to: 'homes#top'
+  root to: 'posts#index'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
