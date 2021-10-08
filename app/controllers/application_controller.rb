6da210749af7ce838f2e8a21b6be364d721b39be
tomomomac37 <tomomomac37@gmail.com>
2021-10-08 22:50:38 +0900

    メインレイアウト適用後

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,10 @@
 # require 'digest/md5'
 class ApplicationController < ActionController::Base
   
-  # before_action :authenticate
+  before_action :authenticate_user!
 
-  # REALM = 'SecretZone'.freeze
-  # USERS = { 'user1' => Digest::MD5.hexdigest(['user1', REALM, 'password'].join(':'))}.freeze
+  REALM = 'SecretZone'.freeze
+  USERS = { 'user1' => Digest::MD5.hexdigest(['user1', REALM, 'password'].join(':'))}.freeze
 
   # private
 
@@ -27,9 +27,7 @@ class ApplicationController < ActionController::Base
     user_path(resource)
   end
   
-  def after_sign_up_path_for(resource)
-    homes_thanks_path(resource)
-  end
+  
   
   
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,11 +11,11 @@ class UsersController < ApplicationController
   
   def edit
     @user = User.find(params[:id])
-    if @user == current_user
-      render "edit"
-    else
-      redirect_to posts_path
-    end
+    # if @user == current_user
+    #   render "edit"
+    # else
+    #   redirect_to posts_path
+    # end
   end
   
   def update
