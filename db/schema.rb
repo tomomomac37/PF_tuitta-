32b6821ca11850482405b73d03662b0d8e2088a8
EC2 Default User <ec2-user@ip-172-31-12-98.ap-northeast-1.compute.internal>
2021-10-30 08:05:13 +0900

    user編集モーダル画面完了

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_28_003000) do
+ActiveRecord::Schema.define(version: 2021_10_16_092044) do
 
   create_table "likes", force: :cascade do |t|
     t.integer "user_id"
@@ -51,8 +51,6 @@ ActiveRecord::Schema.define(version: 2021_10_28_003000) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "profile_image_id"
-    t.string "provider"
-    t.string "uid"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
