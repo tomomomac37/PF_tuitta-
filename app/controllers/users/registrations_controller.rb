07874308b46f6b75303cb594d48ae24f6e1e8796
EC2 Default User <ec2-user@ip-172-31-12-98.ap-northeast-1.compute.internal>
2021-11-28 13:21:10 +0900

    mysql移行前

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -56,7 +56,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # The path used after sign up.
   def after_sign_up_path_for(resource)
-    homes_thanks_path
+    root_path
   end
 
   # The path used after sign up for inactive accounts.
